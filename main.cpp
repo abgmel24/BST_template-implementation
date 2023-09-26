2a2867d689d8b8001abc2ad19b267f91c90694fa
abgmel24 <abhigudimella@gmail.com>
2023-09-26 09:32:34 -0700

    Finished working BST

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "BST.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    BST<int> *t = new BST<int>();
+
+    t->insert(8);
+    t->insert(12);
+    t->insert(10);
+    t->insert(13);
+    t->insert(14);
+    t->insert(4);
+    t->insert(7);
+    t->insert(3);
+    t->insert(5);
+    t->printTree();
+
+    t->remove(12);
+    t->printTree();
+    
+}
